e78881218bf064608399280655904c851a7d11b0
albright <sungutialbright@gmail.com>
2022-06-05 16:03:09 +0300

    ft:add url for index page

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -1,4 +1,11 @@
+from re import U
+from typing import Pattern
+from urllib.parse import urlparse
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from . import views
-from django.urls import path, include
+from django.urls import URLPattern, path, include
+
+urlpatterns=[
+    url(r'^$',views.index,name = 'index'),
+]
