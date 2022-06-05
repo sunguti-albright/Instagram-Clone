2cc691aa319ef0a2ed22e3d4fe7ff55a5037c0dc
albright <sungutialbright@gmail.com>
2022-06-05 16:04:45 +0300

    ft:add view for registration

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+
+@login_required
+def index(request):
+    images = Image.objects.all()
+    comments = Comment.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    if request.method == "POST":
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit = False)
+            image.user = request.user.profile
+            image.save()
+            messages.success(request, f'Successfully uploaded your pic!')
+            return redirect('index')
+    else:
+        form = ImageUploadForm()
+    return render(request, 'index.html', {"images":images[::-1], "form": form, "users": users, "comments": comments })
