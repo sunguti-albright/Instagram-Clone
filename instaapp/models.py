b6a9f16bd203a59d1e4e1e137bfe3df89a42af71
albright <sungutialbright@gmail.com>
2022-06-06 17:52:39 +0300

    create profile model and function to save profile

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import pre_delete
+from PIL import Image
+import cloudinary
+import datetime as dt
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,  related_name='profile')
+    bio = models.TextField()
+    profile_photo = CloudinaryField('profile_photo')
+
+
+    def save_profile(self):
+        self.save()
