eef02a02226808427df8b1c76cb85b3084c15ec1
albright <sungutialbright@gmail.com>
2022-06-06 07:51:32 +0300

    add view functions for login and register pages

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse, Http404
-
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, ImageUploadForm, CommentForm
+from .models import Profile, Image, User, Subscribers, Follow, Comment, Like
+from cloudinary.forms import cl_init_js_callbacks
+from django.core.exceptions import ObjectDoesNotExist
+# from .email import send_welcome_email
 
 @login_required
 def index(request):
@@ -18,3 +24,20 @@ def index(request):
     else:
         form = ImageUploadForm()
     return render(request, 'index.html', {"images":images[::-1], "form": form, "users": users, "comments": comments })
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data['email']
+            recipient = Subscribers(name = username,email =email)
+            recipient.save()
+            # send_welcome_email(username,email)
+            messages.success(request, f'Successfully created account created for {username}! Please log in to continue')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form':form})
+
