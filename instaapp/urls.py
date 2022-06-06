8af0f05ef9f0da07da0fe1fc5e3f52ad308f4d51
albright <sungutialbright@gmail.com>
2022-06-06 20:26:35 +0300

    add url for posting images

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -8,5 +8,9 @@ urlpatterns=[
     url(r'^register/',views.register,name = 'register'),
     url(r'^login/',auth_views.LoginView.as_view(template_name='users/login.html'),name = 'login'),
     url(r'^logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name = 'logout'),
-   
+    url(r'^profile/',views.profile,name = 'profile'),
+    url(r'^update/',views.update,name = 'update'),
+    url(r'^image/(\d+)',views.image,name ='image'),
+    url(r'^search/', views.search_results, name='search_results'),
+  
 ] 
