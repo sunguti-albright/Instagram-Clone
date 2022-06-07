2eb9434aee27ab0da4d5609c44e544df918e5de4
albright <sungutialbright@gmail.com>
2022-06-07 13:15:14 +0300

    register follow and comment models

--- instaapp/admin.py
+++ instaapp/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Profile, Image
+from .models import Profile, Image, Like, Subscribers, Comment, Follow
 
 # Register your models here.
 admin.site.register(Profile)
-# admin.site.register(Image)
-
+admin.site.register(Image)
+admin.site.register(Like)
+admin.site.register(Subscribers)
+admin.site.register(Comment)
+admin.site.register(Follow)
 
