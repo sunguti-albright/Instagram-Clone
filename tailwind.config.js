dd438e505b5ee3ea0a01781d3b755150d71344a8
AlbrightSunguti <sungutialbright@gmail.com>
2023-04-28 15:23:38 +0300

    chore:tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './templates/**/*.html',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 module.exports = {
-  content: [
-    './templates/**/*.html',
+  purge: [
+    './**/*.html',
+    './**/*.js',
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
